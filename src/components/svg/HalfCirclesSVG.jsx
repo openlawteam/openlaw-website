a01f82518e9934351cb6e5f191ee2de2ff399678
Marko <mbajlovic@gmail.com>
2018-07-17 19:25:04 -0400

    unqiue classes in svgs and titles

--- src/components/svg/HalfCirclesSVG.jsx
+++ src/components/svg/HalfCirclesSVG.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HalfCirclesSVG = () => (
-  <div dangerouslySetInnerHTML={{ __html: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 105.8 105.8"><defs><style>  .cls-1{isolation:isolate;}.cls-2{mix-blend-mode:soft-light;}.cls-3,.cls-4{fill:#fff;}.cls-3{opacity:0.5;}</style></defs><title>  Documented-Asset 171</title><g class=""><g data-name="Layer 2"><g data-name="Layer 1"><g class="cls-2"><path class="cls-3" d="M41.4 64.8a29 29 0 0 1 41-41l8.2-8.1A40.8 40.8 0 0 0 32.9 73.4Z"/><path class="cls-4" d="M82.5 23.8a29 29 0 0 1-41 41h0L32.9 73.4A40.8 40.8 0 0 0 90.6 15.7l-8.1 8.2Z"/><path class="cls-3" d="M62.1 96.2A52.4 52.4 0 0 1 25 6.9L18.2 0a62 62 0 0 0 87.7 87.7L99.1 80.9A52.2 52.2 0 0 1 62.1 96.2Z"/></g></g></g></g></svg>'}} />
+  <div dangerouslySetInnerHTML={{ __html: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 105.8 105.8"><defs><style>.cls-1{isolation:isolate;}.cls-2a2gg2fx{mix-blend-mode:soft-light;}.cls-32fa22f,.cls-4{fill:#fff;}.cls-32fa22f{opacity:0.5;}</style></defs><title>HalfCirclesSVG</title><g class=""><g data-name="Layer 2"><g data-name="Layer 1"><g class="cls-2a2gg2fx"><path class="cls-32fa22f" d="M41.4 64.8a29 29 0 0 1 41-41l8.2-8.1A40.8 40.8 0 0 0 32.9 73.4Z"/><path class="cls-4" d="M82.5 23.8a29 29 0 0 1-41 41h0L32.9 73.4A40.8 40.8 0 0 0 90.6 15.7l-8.1 8.2Z"/><path class="cls-32fa22f" d="M62.1 96.2A52.4 52.4 0 0 1 25 6.9L18.2 0a62 62 0 0 0 87.7 87.7L99.1 80.9A52.2 52.2 0 0 1 62.1 96.2Z"/></g></g></g></g></svg>'}} />
 );
 
 export default HalfCirclesSVG;
