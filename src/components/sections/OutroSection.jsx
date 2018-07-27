39340036561fa90c3d9a317de62e02b046a74ace
Marko <mbajlovic@gmail.com>
2018-07-27 13:52:15 -0400

    outro section animation

--- src/components/sections/OutroSection.jsx
+++ src/components/sections/OutroSection.jsx
@@ -12,28 +12,49 @@ import AOS from 'aos';
 import SignupForm from '../../components/SignupForm';
 import Wrap from '../common/Wrap';
 
-const OutroSection = ({ data }) => (
-  <Wrap>
-  	<div className={s.outroSection}>
-	  	<div className={s.largeCircleWrapper}>
-	  		<LargeCircleSVG />
-		</div>
-	  	<div className={`${s.fullRow}`}>
-			<div className={s.columnBg}></div>
-	  		<div className={f.row}>
-			  	<div className={`${f.column} ${f['small12']} ${f['medium6']}`} key="1">
-					<h2 className={`${s.heading}`}>Join us and help build a lower cost and fairer legal system.</h2>
-					<WaveSVG />
-			  	</div>
-			  	<div className={`${f.column} ${f['small12']} ${f['medium6']}`} key="2">
-			  		<div className={s.signUpForm}>
-			    		<SignupForm />
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-  </Wrap>
-);
+class OutroSection extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    AOS.init({
+      duration: 800,
+      offset: 40,
+      delay: 120,
+      mirror: true,
+      once: true,
+      // anchorPlacement: 'top-bottom'
+    })
+  }
+
+  render() {
+    return(
+      <Wrap>
+        <div className={s.outroSection}>
+          <div className={s.largeCircleWrapper} data-aos="fade" data-aos-delay="400">
+            <LargeCircleSVG />
+          </div>
+          <div className={`${s.fullRow}`}>
+            <div className={s.columnBg}></div>
+            <div className={f.row}>
+              <div className={`${f.column} ${f['small12']} ${f['medium6']}`} key="1" data-aos="fade-up" data-aos-delay="200">
+              <h2 className={`${s.heading}`}>Join us and help build a lower cost and fairer legal system.</h2>
+              <WaveSVG />
+              </div>
+              <div className={`${f.column} ${f['small12']} ${f['medium6']}`} key="2" data-aos="fade-up" data-aos-delay="400">
+                <div className={s.signUpForm} data-aos="fade-up" data-aos-delay="600">
+                  <SignupForm />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Wrap>
+    )
+  }
+
+};
 
 export default OutroSection;
