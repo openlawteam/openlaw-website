ddb65203ad328e03bb146745698bf3b38d36bf3a
Jeremiah Trein <jeremiahtrein@gmail.com>
2018-08-10 22:26:51 +0545

    [develop] links edits

--- src/config/nav.js
+++ src/config/nav.js
@@ -7,17 +7,17 @@ const NavData = [
     url: '/private-instances',
   }, {
     name: 'Docs',
-    url: 'https://docs.openlaw.io/',
+    url: 'https://docs.openlaw.io',
   }, {
     name: 'Contact',
     url: 'mailto:hello@openlaw.io',
     divider: true,
   }, {
     name: 'Login',
-    url: 'https://openlaw.io/login',
+    url: 'https://openlaw.io',
   }, {
     name: 'Sign up',
-    url: 'https://openlaw.io/signup',
+    url: 'https://openlaw.io/#register',
   },
 ];
 
