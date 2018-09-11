81b75d376b485f1fad6c3b83bcd21ee927601d2b
Jeremiah Trein <jeremiahtrein@gmail.com>
2018-09-11 08:26:57 +0545

    [automate-section] adds automate section, updates other graphics a bit

---
+++ src/components/svg/AutomateSVG.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const AutomateSVG = () => (
+  <svg height='150' width='150' viewBox='0 0 150 150' xmlns='http://www.w3.org/2000/svg'>
+    <linearGradient id='a' x1='0%' x2='99%' y1='0%' y2='98%'>
+      <stop offset='0' stopColor='#01ffce' />
+      <stop offset='1' stopColor='#0091ff' />
+    </linearGradient>
+    <linearGradient id='b' x1='93%' x2='9%' y1='25%' y2='74%'>
+      <stop offset='0' stopColor='#bfbfff' />
+      <stop offset='1' stopColor='#58fffa' />
+    </linearGradient>
+    <linearGradient id='c' x1='100%' x2='50%' y1='-51%' y2='100%'>
+      <stop offset='0' stopColor='#fff' />
+      <stop offset='1' stopColor='#fff' stopOpacity='0' />
+    </linearGradient>
+    <linearGradient id='d' x1='50%' x2='50%' y1='0%' y2='100%'>
+      <stop offset='0' stopColor='#fad961' />
+      <stop offset='1' stopColor='#f76b1c' />
+    </linearGradient>
+    <linearGradient id='e' x1='0%' x2='102%' y1='0%' y2='101%'>
+      <stop offset='0' stopColor='#3023ae' />
+      <stop offset='1' stopColor='#c86dd7' />
+    </linearGradient>
+    <g fill='none'>
+      <path d='M35 70h19M37 75h18M37 80h16M34 106h8M28 111h15M22 116h22M37 88h17M37 93h12M36 98h16'
+      stroke='#b3acff' />
+      <path d='M11 58L1 47l15-24 14 3c6-5 12-9 20-11l4-14h28l5 14c7 2 14 6 20 11l14-3 14 24-10 10a55 55 0 0 1 0 23l10 10-14 24-15-3c-5 5-12 9-19 11l-5 14H54l-5-14c-7-2-13-6-19-11l-14 3L1 90l10-11a55 55 0 0 1 0-21zm57 42c18 0 32-14 32-31 0-18-14-32-32-32S36 51 36 69c0 17 14 31 32 31z'
+      fill='#fff' stroke='url(#a)' transform='rotate(-120 111 48)' />
+      <circle cx='70' cy='70' r='3' stroke='#ffba00' />
+      <path d='M11 58L1 47l15-24 14 3c6-5 12-9 20-11l4-14h28l5 14c7 2 14 6 20 11l14-3 14 24-10 10a55 55 0 0 1 0 23l10 10-14 24-15-3c-5 5-12 9-19 11l-5 14H54l-5-14c-7-2-13-6-19-11l-14 3L1 90l10-11a55 55 0 0 1 0-21zm57 42c18 0 32-14 32-31 0-18-14-32-32-32S36 51 36 69c0 17 14 31 32 31z'
+      fill='#fff' stroke='url(#a)' transform='rotate(-120 37 19)' />
+      <path d='M99-59h81v189H99z' fill='url(#c)' transform='rotate(-35 139 35)'
+      />
+      <path d='M70 150v-19l16-19V88l18-15' stroke='url(#d)' transform='matrix(1 0 0 -1 0 223)'
+      />
+      <path d='M44 153v-22l18-13v-13l-6-7V64' stroke='url(#d)' />
+      <path d='M36 150v-22l18-13v-10l-4-5M70 150v-48l-7-7V67' stroke='url(#d)'
+      />
+      <path d='M18 28v34l8 8v12L1 96v54' stroke='url(#e)' />
+      <path d='M16 98l17-10V65l-6-8' stroke='url(#e)' />
+      <circle cx='18' cy='25' r='3' stroke='#ffba00' />
+      <path d='M15 134v-25l17-10' stroke='url(#e)' />
+      <circle cx='27' cy='54' r='3' stroke='#ffba00' />
+      <path d='M46 139h22M11 139h22M8 87V48h8M23 48h55v48M78 106v29M8 95v44'
+      stroke='#07f' />
+    </g>
+  </svg>
+);
+
+export default AutomateSVG;
