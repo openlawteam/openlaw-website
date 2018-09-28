437d7849e42baf5ce86a776a98b677182034fd83
Ramiro Jr. Franco <rjfranco@gmail.com>
2018-09-28 12:35:27 -0700

    Adding case for staging urls

--- src/helper/url.js
+++ src/helper/url.js
@@ -27,6 +27,8 @@ export const websiteURL = () => {
       return 'https://develop.dev.openlaw.io/';
     case 'production_production':
       return 'https://openlaw.io/';
+    case 'production_staging':
+      return 'https://staging.dev.openlaw.io';
     default:
       return 'http://localhost:8000/';
   }
