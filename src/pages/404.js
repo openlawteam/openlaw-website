2d544ee47d09a9766056f79388de698e33bf3be8
RunRafter <tcai534@gmail.com>
2018-12-12 17:47:42 -0500

    Built basics of new 404 page, but problem with svg

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import Layout from '../components/layout'
+import { Link } from 'gatsby';
+import s from '../scss/modules/errorpage.module.scss';
+import { LogoCircle } from '../components/svg/LogoCircle';
+import { PizzaDogSVG } from '../components/svg/PizzaDogSVG'
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+	<div className={s.errorPage}>
+	  <div className={s.mainName}>
+	     <Link to="/"><LogoCircle className={s.mainLogo}/></Link>
+	  </div>
+	  <div className={s.errorPageTitle}>
+	    We couldn't find this page.
+	  </div>
+	  <div>
+	  	<p>You may have typed an invalid URL.</p>
+	    <p>
+	      <PizzaDogSVG/>
+	    </p>
+	  </div>
+	  <div className="error-page-links">
+	    Here are a few links that may be helpful:
+	    <br />
+	    <ul>
+	      <li><a href="/">Home</a></li>
+	      <span className="error-page-links-divider">|</span>
+	      <li><a href="/faq">FAQ</a></li>
+	    </ul>
+	  </div>
+	</div>
+
 )
 
 export default NotFoundPage
