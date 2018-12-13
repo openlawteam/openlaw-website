4d7719f2a77a19fbeeb00916cb53b43c2bca3f54
Tony Cai <tcai534@gmail.com>
2018-12-13 00:06:39 -0500

    Style 404 on Static Page (#78)
    * Built basics of new 404 page, but problem with svg
    
    * Fixed remaining issues
    
    * [style-404] adds fonts, removes dead page
    
    * [style-404] fixes spacing
    
    * [style-404] fixes an HTML unescaped character in JSX

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,36 @@
 import React from 'react'
-import Layout from '../components/layout'
+import { Link } from 'gatsby';
+import 'typeface-hind';
+import 'typeface-nunito';
+
+import s from '../scss/modules/errorpage.module.scss';
+import { LogoCircle } from '../components/svg/LogoCircle';
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <div className={s.errorPage}>
+    <div className={s.mainName}>
+       <Link to="/"><LogoCircle className={s.mainLogo}/></Link>
+    </div>
+    <div className={s.errorPageTitle}>
+      We couldn&rsquo;t find this page.
+    </div>
+    <div>
+      <p>You may have typed an invalid URL.</p>
+      <p>
+        <img className={s.errorImage} src="/static/img/pizza-dog-optimized.svg" />
+      </p>
+    </div>
+    <div className={s.errorPageLinkSection}>
+      Here are a few links that may be helpful:
+      <br />
+      <ul className={s.errorPageLinkList}>
+        <li className={s.errorPageLinkItem}><a href="/" className={s.errorPageLink}>Home</a></li>
+        <span className={s.errorPageLinksDivider}>|</span>
+        <li className={s.errorPageLinkItem}><a href="/faq" className={s.errorPageLink}>FAQ</a></li>
+      </ul>
+    </div>
+  </div>
+
 )
 
 export default NotFoundPage
